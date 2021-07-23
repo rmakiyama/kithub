5de2702b208a5575600f0ebbd90d844a60656c4e
rmakiyama <rmakiyama.io@gmail.com>
2021-07-23 18:45:19 +0900

    initial commit

---
+++ shared/src/androidMain/kotlin/com/rmakiyama/kithub/Platform.kt
@@ -0,0 +1,5 @@
+package com.rmakiyama.kithub
+
+actual class Platform actual constructor() {
+    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
---
+++ shared/src/commonMain/kotlin/com/rmakiyama/kithub/Platform.kt
@@ -0,0 +1,5 @@
+package com.rmakiyama.kithub
+
+expect class Platform() {
+    val platform: String
+}
