aa9c840519b2ccd54ce84b2875dd10fb6bbe5df8
rmakiyama <rmakiyama.io@gmail.com>
2021-07-24 20:03:08 +0900

    ktsの非推奨箇所修正

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,17 +5,17 @@ plugins {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     defaultConfig {
-        applicationId = "com.rmakiyama.kithub.android"
-        minSdkVersion(23)
-        targetSdkVersion(30)
+        applicationId = "com.rmakiyama.kithub"
+        minSdk = 23
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
